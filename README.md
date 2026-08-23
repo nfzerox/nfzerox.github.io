fe996c7fe13b4baa052c9787a8f73b412e7216b7
nfzerox <nfzerox@protonmail.com>
2026-08-22 23:38:06 -0500

    Update repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# nfzerox.github.io
--- cydia/update.sh
+++ cydia/update.sh
@@ -89,7 +89,7 @@ cat > "$REL" <<EOF
 Origin: nfzerox
 Label: nfzerox
 Suite: stable
-Version: 1.1.2
+Version: 1.2
 Codename: ios
 Architectures: iphoneos-arm iphoneos-arm64
 Components: main
